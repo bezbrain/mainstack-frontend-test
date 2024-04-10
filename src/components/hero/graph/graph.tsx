941e22cedde293fbf605fc8841f9ccfcf6ca464f
bezbrain <belloquadriolawale@gmail.com>
2024-04-10 18:39:47 +0100

    Design the chart UI

--- src/components/hero/graph/graph.tsx
+++ src/components/hero/graph/graph.tsx
@@ -1,7 +1,26 @@
+import { TheChart } from "..";
+import { Button } from "../../general/button";
+
 const Graph = () => {
   return (
-    <div className="border-2 min-h-[300px] iPadAir:w-[60%]">
-      <p>Graph Section</p>
+    <div className="iPadAir:w-[60%]">
+      <div className="iPhone:flex items-center gap-10 surfaceDuo:gap-20">
+        <div>
+          <p className="text-[14px] surfaceDuo:text-[16px]">
+            Available Balance
+          </p>
+          <h2 className="text-[20px] surfaceDuo:text-[36px] text-[#000] font-extrabold">
+            USD 120,500.00
+          </h2>
+        </div>
+        <Button
+          btnContent="Withdraw"
+          btnStyle="py-2 px-10 bg-[#3b4147] text-[#f1f1f1] my-2 iPhone:my-0"
+        />
+      </div>
+      <div className="overflow-x-auto">
+        <TheChart />
+      </div>
     </div>
   );
 };
